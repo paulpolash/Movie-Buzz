b05afbca9e1ca12ce56eb24c54260b99fc82f043
paulpolash <premacaitanyapranadas@gmail.com>
2026-09-20 12:12:34 +0600

    resolved ksp, room dependency issue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,9 +61,9 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
-    implementation("androidx.room:room-runtime:2.6.1")
-    ksp("androidx.room:room-compiler:2.6.1")
-    implementation("androidx.room:room-ktx:2.6.1")
+    implementation("androidx.room:room-runtime:2.7.0")
+    ksp("androidx.room:room-compiler:2.7.0")
+    implementation("androidx.room:room-ktx:2.7.0")
 
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
